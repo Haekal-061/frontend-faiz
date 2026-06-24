bd06f36f6a40ed8bffd5102f8c9b496688b4dbd5
Haekal-061 <muhhaekal26@gmail.com>
2026-06-24 19:52:01 +0800

    update railway

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({
   baseURL:
     process.env.REACT_APP_API_BASE_URL ||
-    'https://backend-campbread-production.up.railway.app/api'
+    'https://graceful-prosperity-production-1e95.up.railway.app/api'
 });
 
 API.interceptors.request.use((config) => {
