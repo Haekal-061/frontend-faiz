5b240be72f2344816501ae19f7baa44b6c402707
Faizz-321 <muhfaiz030705@gmail.com>
2026-06-24 19:32:29 +0800

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL:
+    process.env.REACT_APP_API_BASE_URL ||
+    'https://backend-campbread-production.up.railway.app/api'
 });
 
 API.interceptors.request.use((config) => {
